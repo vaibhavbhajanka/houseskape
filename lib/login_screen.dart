ff0d89700ca983e5efeea300385b34dd72000673
vaibhavbhajanka <vaibhavbhajanka@gmail.com>
2022-04-05 06:08:47 +0530

    Configured Bottom-Nav-Bar & App-Bar

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -253,7 +253,7 @@ class _LoginScreenState extends State<LoginScreen> {
             .signInWithEmailAndPassword(email: email, password: password)
             .then((uid) => {
                   Fluttertoast.showToast(msg: "Login Successful"),
-                  Navigator.pushReplacementNamed(context, '/home'),
+                  Navigator.pushReplacementNamed(context, '/dashboard'),
                 });
       } on FirebaseAuthException catch (error) {
         switch (error.code) {
